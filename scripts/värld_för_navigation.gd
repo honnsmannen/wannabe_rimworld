60a92a7e23168041e78e1982a9ee51ce5890dd29
honnsmannen <honnsmannen@gmail.com>
2023-03-13 17:49:52 +0100

    mergad version mellan mig och alex

--- scripts/värld_för_navigation.gd
+++ scripts/värld_för_navigation.gd
@@ -9,6 +9,9 @@ var level_navigation_map
 #var tree_offset = Vector2(32,32)
 var tree_pos := Vector2(0,0)
 var bush_pos := Vector2(0,0)
+var not_tree_list = []
+var tree_list = []
+
 
 
 var previous_left_mouse_click_global_position : Vector2
@@ -16,6 +19,8 @@ var previous_right_mouse_click_global_position : Vector2
 
 onready var character = $KinematicBody2D
 onready var parent_level_scene = ("res://scener/värld_för_navigation.tscn")
+onready var filuren = $Filuren
+
 
 var characters = []
 
@@ -36,55 +41,11 @@ func _ready() -> void:
 	noise.period = 15
 	
 	noise.octaves = 1
-	
-	for i in 50:
-		cell_x += 1
-		cell_y = 0
-		for j in 50:
-			cell_y += 1
-			
-			var cell_xy = Vector2(cell_x, cell_y)
-			
-			#fixar så att det inte finns -1 i värld genen
-			if int(round(noise.get_noise_2d(cell_x, cell_y))) < 0:
-				compenserat_value = int(round(noise.get_noise_2d(cell_x, cell_y))) * -1
-			#ett lätt sätt att få ut slumpade platser på träd
-			elif randi() % 14 == 1 and compenserat_value != 1:
-				tree_pos = Vector2(cell_xy * 32)
-				var nytree = tree.instance()
-				nytree.global_position = tree_pos
-				add_child(nytree)
-			elif randi() % 40 == 1 and compenserat_value != 1:
-				bush_pos = Vector2(cell_xy * 32)
-				var nybush = Bush.instance()
-				nybush.global_position = bush_pos
-				add_child(nybush)
-			else:
-				compenserat_value = int(round(noise.get_noise_2d(cell_x, cell_y)))
-			tile.set_cellv(cell_xy, compenserat_value)
-			#print(tile.get)
-			#tile.set_cellv(cell_xy, int(round(noise.get_noise_2d(cell_x, cell_y))))
-			
+
 	
 func _process(delta: float) -> void:
 	update()
-	"""
-	if Input.is_action_just_pressed("click"):
-		
-		var instanced_in_code := true
-		var previous_left_mouse_click_global_position = get_global_mouse_position()
-		#character.init_character(parent_level_scene, instanced_in_code)
-		character.set_navigation_position(get_global_mouse_position())
-	"""
-
-"""
-func Hantera_left_click() -> void:
-	
-	previous_left_mouse_click_global_position = get_global_mouse_position()
-	# set all level characters new navigation position
-	for character in characters:
-		character.set_navigation_position(get_global_mouse_position())
-"""
+	world_gen(40, 40)
 func init_pre_existing_level_characters() -> void:
 	# init all the character scenes in the scene tree when starting the level
 	# other characters created in create_character() will be initilized at that time
@@ -97,55 +58,41 @@ func init_pre_existing_level_characters() -> void:
 				child_node.init_character(self, false)
 				characters.push_back(child_node)
 
-"""
-func _draw() -> void:
-	# TODO: draw needs some clean up and has some draw errors
-	# Error: canvas_item_add_polygon: Invalid polygon data, triangulation failed.
-	for character in characters:
-		if character is KinematicBody2D and is_instance_valid(character) and character.is_inside_tree():
-			if character.character_nav_path.size() > 1:
-				var previous_line_point : Vector2 = character.character_nav_path[1]
-				for path_index in range(1, character.character_nav_path.size()):
-					var line_point : Vector2 = character.character_nav_path[path_index]
-					if previous_line_point is Vector2 and line_point is Vector2 and previous_line_point.distance_to(line_point) > 2.0:
-						draw_line(previous_line_point, line_point, Color(1.0, 0.3, 0.7, 1.0), 3.0, false)
-						draw_circle(line_point, 4.0, Color(0.1, 5.0, 0.6, 1.0))
-					previous_line_point = line_point
+func world_gen(width: int, height: int) -> void:
+	var center = filuren.global_position
+	var start_x = int(center.x / 32) - width/2
+	var start_y = int(center.y / 32) - height/2
+	for x in range(start_x, start_x + width):
+		for y in range(start_y, start_y + height):
+			
+			# Generate world data at (x, y)
+			var noise_x = x
+			var noise_y = y
+			var noise_value = noise.get_noise_2d(noise_x, noise_y)
+			
+			"""
+			den kommande biten skrev jag
+			"""
+			if noise_value < 0:
+				compenserat_value = int(round(noise_value)) * -1
 
-
-			if character.character_real_nav_path.size() > 1:
-				var previous_line_point : Vector2 = character.character_real_nav_path[0]
-				for path_index in range(1, character.character_real_nav_path.size()):
-					var line_point : Vector2 = character.character_real_nav_path[path_index]
-					if previous_line_point is Vector2 and line_point is Vector2 and previous_line_point.distance_to(line_point) > 2.0:
-						draw_line(previous_line_point, line_point, Color(0.6, 0.6, 0.2, 1.0), 3.0, false)
-						draw_circle(line_point, 4.0, Color(0.3, 0.8, 0.2, 1.0))
-					previous_line_point = line_point
-			
-			draw_circle(character.next_nav_position, 10.0, Color(0.5, 1.0, 0.1, 1.0))
-			if character.global_position.distance_to(character.next_nav_position) > 1.0:
-				draw_line(character.global_position, character.next_nav_position, Color(1.0, 0.6, 0.8, 1.0), 5.0, false)
-			
-			draw_circle(character.global_position, character.nav_agent.radius, Color(1.0, 0.0, 0.3, 1.0))
-			
-			draw_circle(character.nav_destination, character.nav_agent.radius, Color(0.7, 0.5, 0.2, 1.0))
-			
-			if character.global_position.distance_to(character.velocity) > 2.0:
-				draw_line(character.global_position, character.global_position + character.velocity, Color(0.3, 0.5, 1.0, 1.0), 3.0, false)
-			draw_circle(character.global_position + character.velocity, 5.0, Color(0.2, 0.5, 0.7, 1.0))
-"""
-"""
-	for obstacle in obstacles:
-		if obstacle is Node2D and is_instance_valid(obstacle) and obstacle.is_inside_tree():
-			if obstacle.nav_obstacle.estimate_radius:
-				draw_circle(obstacle.global_position, obstacle.collision_radius, Color(0.5, 0.6, 0.4, 1.0))
 			else:
-				draw_circle(obstacle.global_position, obstacle.obstacle_nav_radius, Color(0.5, 0.6, 0.4, 1.0))
-	
-	if level_camera is Camera2D and is_instance_valid(level_camera) and level_camera.is_inside_tree():
-		draw_line(level_camera.global_position, level_camera.camera_target_position, Color(0.3, 0.7, 0.1, 1.0), false)
-"""
+				compenserat_value = int(round(noise_value))
+			
+			if not randi() % 14 == 1 and compenserat_value != 1:
+				if not Vector2(x,y) in not_tree_list:
+					not_tree_list.append(Vector2(x,y))
+			
+			elif randi() % 14 == 1 and compenserat_value != 1:
+				if not Vector2(x,y) in tree_list and not Vector2(x,y) in not_tree_list:
+					tree_pos = Vector2(x * 32, y * 32)
+					tree_list.append(Vector2(x,y))
+					print(tree_list)
+					var nytree = tree.instance()
+					nytree.global_position = tree_pos
+					add_child(nytree)
+			
+				
+			
+			$TileMap.set_cell(x, y, int(compenserat_value))
 
-
-func _on_TextureButton_pressed():
-	pass # Replace with function body.
